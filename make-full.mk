f71d018977bf68f9a772cc144c2528904993002f
jrom876 <jrom876@gmail.com>
2025-03-05 09:25:30 -0700

    Adding mathCalc to make-full.mk

--- make-full.mk
+++ make-full.mk
@@ -14,10 +14,22 @@ LIBS=-lm -lcheck -lpthread -lrt -lsubunit -lcheck_pic
 ## `pkg-config --cflags gtk+-2.0`
 
 # Dependencies
-DEPS = mathCalc.c mathCalc.h
-OBJ = mathCalc.o mathCalc.i mathCalc.s mathCalc.pie mathCalc.hex
+DEPS =  complexCalc.c complexCalc.h \
+	mathCalc.c mathCalc.h
+OBJ = 	mathCalc.o mathCalc.i mathCalc.s mathCalc.pie mathCalc.hex \
+	complexCalc.o complexCalc.i complexCalc.s complexCalc.pie complexCalc.hex
 
-all: mathCalc
+all: complexCalc mathCalc
+
+complexCalc: complexCalc.o
+	$(CC) -o complexCalc complexCalc.o $(LIBS)
+
+complexCalc.o: $(DEPS)
+	$(CC) $(CFLAGS) -c complexCalc.c $(LIBS)
+	$(CC) -E complexCalc.c -o complexCalc.i $(LIBS)
+	$(CC) -S complexCalc.c -fverbose-asm -o complexCalc.s $(LIBS)
+	$(CC) -pie complexCalc.c -o complexCalc.pie $(LIBS)
+	objcopy -O ihex complexCalc complexCalc.hex
 
 mathCalc: mathCalc.o
 	$(CC) -o mathCalc mathCalc.o $(LIBS)
